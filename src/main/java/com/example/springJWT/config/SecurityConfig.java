46ccca716c11bb5497edf4939546f75b220a09e1
220919haeundae <113912025+220919haeundae@users.noreply.github.com>
2024-12-10 19:53:36 +0900

    feat: spring security jwt user login 전반부 학습  - UsernamePasswordAuthenticationFilter 커스텀    - securityConfig에 커스텀 필터(LoginFilter) 등록    - 주요 내용: UsernamePasswordAuthenticationFilter.attemptAuthentication(...) 오버라이딩

--- src/main/java/com/example/springJWT/config/SecurityConfig.java
+++ src/main/java/com/example/springJWT/config/SecurityConfig.java
@@ -1,20 +1,38 @@
 package com.example.springJWT.config;
 
+import com.example.springJWT.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -36,6 +54,8 @@ public class SecurityConfig {
                 .requestMatchers("/login", "/", "join").permitAll()
                 .requestMatchers("/admin").hasRole("/admin")
                 .anyRequest().authenticated());
+
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
