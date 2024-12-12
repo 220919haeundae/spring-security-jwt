c161203a47725ead508206e65deafa0038a485c0
220919haeundae <113912025+220919haeundae@users.noreply.github.com>
2024-12-12 21:32:29 +0900

    feat: refresh 토큰 DB 저장 로직 추가  - 이후 Scheduler 활용 DB에서 만료기한 초과한 토큰 삭제 로직 구현 예정

--- src/main/java/com/example/springJWT/config/SecurityConfig.java
+++ src/main/java/com/example/springJWT/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springJWT.config;
 import com.example.springJWT.jwt.JWTFilter;
 import com.example.springJWT.jwt.JWTUtil;
 import com.example.springJWT.jwt.LoginFilter;
+import com.example.springJWT.repository.RefreshRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,10 +29,12 @@ public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil, RefreshRepository refreshRepository) {
         this.authenticationConfiguration = authenticationConfiguration;
         this.jwtUtil = jwtUtil;
+        this.refreshRepository = refreshRepository;
     }
 
     @Bean
@@ -86,7 +89,7 @@ public class SecurityConfig {
         http
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
--- src/main/java/com/example/springJWT/controller/ReissueController.java
+++ src/main/java/com/example/springJWT/controller/ReissueController.java
@@ -1,6 +1,8 @@
 package com.example.springJWT.controller;
 
+import com.example.springJWT.entity.RefreshEntity;
 import com.example.springJWT.jwt.JWTUtil;
+import com.example.springJWT.repository.RefreshRepository;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReissueController {
 
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
-    public ReissueController(JWTUtil jwtUtil) {
+    public ReissueController(JWTUtil jwtUtil, RefreshRepository refreshRepository) {
         this.jwtUtil = jwtUtil;
+        this.refreshRepository = refreshRepository;
     }
 
     @PostMapping("/reissue")
@@ -47,9 +51,19 @@ public class ReissueController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("refresh token expired");
         }
 
+        // 토큰이 refresh 토큰인지 확인 (발급시 페이로드에 명시)
         String category = jwtUtil.getCategory(refresh);
 
         if(!category.equals("refresh")) {
+            // response status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid refresh token");
+        }
+
+        // 토큰이 DB에 저장되어 있는지 확인
+        Boolean isExist = refreshRepository.existByRefresh(refresh);
+
+        if(!isExist) {
+            // response body
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid refresh token");
         }
 
@@ -59,6 +73,10 @@ public class ReissueController {
         // make new JWT
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
         String newRefresh = jwtUtil.createJwt("refresh", username, role, 24*6*600000L);
+
+        // Refresh 토큰 저장 DB에 기존 Refresh 토큰 삭제 후 새 Refresh 토큰 저장
+        refreshRepository.deleteByRefresh(refresh);
+        addRefreshEntity(username, refresh, 24*6*600000L);
 
         // response
         response.setHeader("access", newAccess);
@@ -75,6 +93,18 @@ public class ReissueController {
         cookie.setHttpOnly(true);
 
         return cookie;
+    }
+
+    private void addRefreshEntity(String username, String refresh, long l) {
+
+        RefreshEntity refreshEntity = new RefreshEntity();
+
+        refreshEntity.setUsername(username);
+        refreshEntity.setRefresh(refresh);
+        refreshEntity.setExpiration(String.valueOf(l) + System.currentTimeMillis());
+
+        refreshRepository.save(refreshEntity);
+
     }
 
 }
