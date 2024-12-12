8eebc3a2cef02a234e13b7161f2d94dde8374977
220919haeundae <113912025+220919haeundae@users.noreply.github.com>
2024-12-12 20:31:34 +0900

    feat: refresh token 검증 컨트롤러 구현

---
+++ src/main/java/com/example/springJWT/controller/ReissueController.java
@@ -0,0 +1,69 @@
+package com.example.springJWT.controller;
+
+import com.example.springJWT.jwt.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReissueController {
+
+    private final JWTUtil jwtUtil;
+
+    public ReissueController(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        // get refresh token
+        String refresh = null;
+
+        Cookie[] cookies = request.getCookies();
+
+        for(Cookie cookie : cookies) {
+            if(cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+            }
+        }
+
+        // null check
+        if(refresh == null) {
+            //response status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("refresh token null");
+        }
+
+        try {
+
+            jwtUtil.isExpired(refresh);
+
+        }catch(ExpiredJwtException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("refresh token expired");
+        }
+
+        String category = jwtUtil.getCategory(refresh);
+
+        if(!category.equals("refresh")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid refresh token");
+        }
+
+        String username = jwtUtil.getUsername(refresh);
+        String role = jwtUtil.getRole(refresh);
+
+        // make new JWT
+        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+
+        // response
+        response.setHeader("access", newAccess);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+}
